96e5b89768d630ca16c3fd7e5d012bee4d795b6e
Mafham <samuelmafham@hotmail.co.uk>
2023-12-28 13:16:39 +0000

    Update Valuable Drop Recolor

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Example
-An example greeter plugin
+# Valuable Drop Recolor
+Changes the color of loot drop notifications based on their value. Loot drop notifications must be enabled in the in-game settings for this to work
+
+![example](https://i.imgur.com/kJtLDvW.png)
--- src/test/java/com/valuabledroprecolor/ValuableDropRecolorPluginTest.java
+++ src/test/java/com/valuabledroprecolor/ValuableDropRecolorPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.valuabledroprecolor;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ValuableDropRecolorPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ValuableDropRecolorPlugin.class);
 		RuneLite.main(args);
 	}
 }
